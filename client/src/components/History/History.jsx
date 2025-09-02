216cbc6d5eff6d905cd79a362308b4163e786462
JP <jpe911@gmail.com>
2025-09-02 09:49:55 -0400

    Render history in reverse chronological order

--- client/src/components/History/History.jsx
+++ client/src/components/History/History.jsx
@@ -6,7 +6,7 @@ function History({ history }) {
   return (
     <section className="history">
       <h2>History</h2>
-      {history.map((entry, idx) => (
+      {history.slice().reverse().map((entry, idx) => (
         <div key={idx} className="history-entry">
           <h3>Detected Language: {entry.output.detectedLanguage.join(', ')}</h3>
           <div className="input-text">
@@ -15,7 +15,7 @@ function History({ history }) {
           <div className="tokenized-text">
             {entry.output.tokenizedText.map((token, index) => (
               <div key={index} className="token">
-                <strong>{token.word}</strong>: {token.definition}
+                <strong>{token.word}</strong>: {token.definition} <em>({token.pronunciation})</em>
               </div>
             ))}
           </div>
